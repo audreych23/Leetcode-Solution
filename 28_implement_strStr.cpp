436c0d22605cfe56fdd05828a37f5f6df90b9855
audreych <audreychung2352002@gmail.com>
2022-07-26 20:27:12 +0800

    added KMP solution

--- 28_implement_strStr.cpp
+++ 28_implement_strStr.cpp
@@ -21,3 +21,49 @@ public:
     return -1;
   }
 };
+
+/**
+ * KMP algorithm
+ * Time Complexity : O(m + n)
+ * Space Complexity : O(m) // m is the needle size
+ */
+ class Solution {
+ public:
+   int strStr(string haystack, string needle) {
+     if (needle.empty()) return 0;
+     if (needle.length() > haystack.size()) return -1;
+     vector<int> pi(int(needle.size()));
+     // pre calculate longest proper prefix which is also a suffix
+     for (int i {1}; i < needle.size(); ++i) {
+      int j { pi[i - 1] };
+      while (j > 0 && needle[i] != needle[j]) {
+        j = pi[j - 1];
+      }
+      if (needle[i] == needle[j]) ++j;
+      pi[i] = j;
+     }
+
+     int n { int(haystack.size()) };
+     int m { int(needle.size()) };
+     int i {}, j {};
+     // find the index of a substring occurence
+     while ((n - i) >= (m - j)) {
+      if (haystack[i] == needle[j]) {
+        ++i;
+        ++j;
+      }
+
+      if (j == m) {
+        return i - j;
+      }
+      if (i < n && haystack[i] != needle[j]) {
+        if (j != 0) {
+          j = pi[j - 1];
+        } else {
+          ++i;
+        }
+      }
+     }
+     return -1;
+   }
+ };
