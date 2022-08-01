74d56c349585a94f6d7505feb719df07a9b8ea4b
audreych <audreychung2352002@gmail.com>
2022-08-02 01:22:44 +0800

    added bitmask solution

--- 2351_first_letter_to_appear_twice.cpp
+++ 2351_first_letter_to_appear_twice.cpp
@@ -1,6 +1,6 @@
 /**
  * Time Complexity: O(n)
- * Space Complexity: O(1)
+ * Space Complexity: O(26)
  */
 class Solution {
 public:
@@ -9,6 +9,26 @@ public:
     for (int i {}; i < int(s.size()); ++i) {
       if (alph[s[i] - 'a']) return s[i];
       alph[s[i] - 'a']++;
+    }
+    return 'a'; // will not happen
+  }
+};
+
+/**
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+class Solution {
+public:
+  char repeatedCharacter(string s) {
+    // char 'a' -> 0th bit is flipped, 'b' -> 1st bit, 'c' -> 2nd, etc
+    // if 'a' and 'd' is met it's gonna be 1001 in bit -> val = 9
+    int val {};
+    for (int i {}; i < int(s.size()); ++i) {
+      // check if the bit is flipped to 1 by shifting right by s[i] bit where a -> 0, b -> 1
+      if ((val >> (s[i] - 'a')) & 1) return s[i];
+      // flip the bit in the corresponding position depending on s[i]
+      val = val | (1 << (s[i] - 'a'));
     }
     return 'a'; // will not happen
   }
