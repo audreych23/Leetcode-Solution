7c5a679c5f6227ac3031919ab09245bcf8f98209
audreych <audreychung2352002@gmail.com>
2022-08-27 22:19:41 +0800

    added a very clean solution

--- 112_path_sum.cpp
+++ 112_path_sum.cpp
@@ -33,3 +33,15 @@ public:
     return is_reachable;
   }
 };
+
+/**
+ * Very clean code
+ */
+class Solution {
+public:
+  bool hasPathSum(TreeNode* root, int targetSum) {
+    if (root == nullptr) return false;
+    if (root->val == targetSum && root->left == nullptr && root->right == nullptr) return true;
+    return hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val);
+  }
+};
