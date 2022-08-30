4adc1ec3122fd10f2b80a07293ba9abdee4b3220
audreych <audreychung2352002@gmail.com>
2022-08-31 01:54:31 +0800

    added a cleaner solution

--- 102_binary_tree_level_order_traversal.cpp
+++ 102_binary_tree_level_order_traversal.cpp
@@ -1,6 +1,7 @@
 /**
  * Time complexity: O(n)
  * Space complexity: O(n)
+ * store additional variable like a level in the pair
  */
 /**
  * Definition for a binary tree node.
@@ -39,6 +40,50 @@ public:
     }
     // final push
     ans.push_back(node_same_level);
+    return ans;
+  }
+};
+
+/**
+ * A cleaner solution, it has the same idea as putting a null as an indication when should we go to next lvl
+ * ref-link: (putting this here because the answers discussion are interesting)
+ * https://leetcode.com/problems/binary-tree-level-order-traversal/discuss/33443/C%2B%2B-solution-using-only-one-queue-use-a-marker-NULL
+ * the answer on this discussion is where the idea came from
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+  vector<vector<int>> levelOrder(TreeNode* root) {
+    vector<vector<int>> ans;
+    queue<TreeNode*> q;
+    q.push(root);
+    while (q.size()) {
+      int n = q.size(); // how many elements do in one level at this moment
+      vector<int> node_same_level;
+      // the queue at this point will only store all the elements in a level
+      // make a vector for this level while pushing children into queue
+      for (int i {}; i < n; ++i) {
+        auto front = q.front(); q.pop();
+        if (front != nullptr) {
+          node_same_level.push_back(front->val);
+          q.push(front->left);
+          q.push(front->right);
+        }
+      }
+      // important because all children can be nullptr
+      if (node_same_level.size() > 0)
+        ans.push_back(node_same_level);
+    }
     return ans;
   }
 };
